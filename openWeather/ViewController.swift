bec47bf882254e2469acc01e42c4aa362a21612e
Jeremy Benson <jeremy@ashleybensonfitness.com>
2016-09-05 20:47:19 -0500

    Initial Commit

---
+++ openWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  openWeather
+//
+//  Created by 840west on 9/5/16.
+//  Copyright © 2016 840west. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
